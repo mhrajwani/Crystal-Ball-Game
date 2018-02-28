7a066338cbbb6e60151efa5a91e8f97ff2ac62e9
mhrajwani <mo.rajwani@gmail.com>
2018-02-28 10:49:49 -0500

    Complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,10 +12,10 @@ function reset(){
 
 guess = randomNumber(19,120);
 crystal = 0;
-blue = 0;
-yellow = 0;
-pink = 0;
-red = 0;
+blue = randomNumber(1,12);
+yellow = randomNumber(1,12);
+pink = randomNumber(1,12);
+red = randomNumber(1,12);
 document.getElementById("rNumber").innerHTML = "Random Number is : " + guess;
 document.getElementById("cScore").innerHTML = crystal;
 }
@@ -27,7 +27,7 @@ function lossx(){
 }
 
 function winx(){
-    alert("YOU WON, PLAY AGAIN?")
+    alert("YOU WON, PLAY AGAIN?");
     wins++;
     document.getElementById("wins").innerHTML = "Wins " + wins;
 
@@ -39,110 +39,45 @@ function randomNumber(min,max)
 }
 
 var guess = randomNumber(19,120);
+blue = randomNumber(1,12);
+yellow = randomNumber(1,12);
+pink = randomNumber(1,12);
+red = randomNumber(1,12);
+
+function crystalx(x){
+    crystal = crystal + x;
+    document.getElementById("cScore").innerHTML = crystal;
+    if (guess === crystal){
+        winx();
+        reset();
+    }else if (guess < crystal){
+        lossx();
+        reset();
+    }
+    }
 
 
 
 $(".blue").on("click", function () {
-    
-    if (blue === 0){
-        blue = randomNumber(1,12);
-        crystal = crystal + blue;
-        document.getElementById("cScore").innerHTML = crystal;
-        if (guess === crystal){
-            winx();
-            reset();
-        }else if (guess < crystal){
-            lossx();
-            reset();
-        }
-    } else {
-    crystal = crystal + blue;
-    document.getElementById("cScore").innerHTML = crystal;
-    if (guess === crystal){
-        winx();
-        reset();
-    }else if (guess < crystal){
-        lossx();
-        reset();
-    }
-    }});
+    crystalx(blue);
+    console.log("blue is : " +blue);
+});
 
 $(".red").on("click", function () {
-    if (red === 0){
-        red = randomNumber(1,12);
-        crystal = crystal + red;
-        document.getElementById("cScore").innerHTML = crystal;
-        if (guess === crystal){
-            winx();
-            reset();
-        }else if (guess < crystal){
-            lossx();
-            reset();
-        }
-    } else {
-    crystal = crystal + red;
-    document.getElementById("cScore").innerHTML = crystal;
-    if (guess === crystal){
-        winx();
-        reset();
-    }else if (guess < crystal){
-        lossx();
-        reset();
-    }
-    }});
+    crystalx(red);
+    console.log("red is : " + red);
+});
 
 $(".yellow").on("click", function () {
-    if (yellow === 0){
-        yellow = randomNumber(1,12);
-        crystal = crystal + yellow;
-        document.getElementById("cScore").innerHTML = crystal;
-        if (guess === crystal){
-            winx();
-            reset();
-        }else if (guess < crystal){
-            lossx();
-            reset();
-        }
-    } else {
-    crystal = crystal + yellow;
-    document.getElementById("cScore").innerHTML = crystal;
-    if (guess === crystal){
-        winx();
-        reset();
-    }else if (guess < crystal){
-        lossx();
-        reset();
-
-    }
-    }});
+    crystalx(yellow);
+    console.log("yellow is :" + yellow);
+});
 $(".pink").on("click", function () {
-    if (pink === 0){
-        pink = randomNumber(1,12);
-        crystal = crystal + pink;
-        document.getElementById("cScore").innerHTML = crystal;
-        if (guess === crystal){
-            winx();
-            reset();
-        }else if (guess < crystal){
-            lossx();
-            reset();
-        }
-    } else {
-    crystal = crystal + pink;
-    document.getElementById("cScore").innerHTML = crystal;
-    if (guess === crystal){
-        winx();
-        reset();
-    }else if (guess < crystal){
-        lossx();
-        reset();
-    }
-    }});
+    crystalx(pink);
+    console.log("pink is : " + pink)
+});
 
 
 
 
 document.getElementById("rNumber").innerHTML = "Random Number is : " + guess;
-document.getElementById("cScore").innerHTML = crystal;
-document.getElementById("wins").innerHTML = "Wins " + wins;
-document.getElementById("loss").innerHTML = "Losses " + loss;
